672bd60dde3828c55d203e7152a16bfc7f3a8660
Aaron Relph <imgonnarelph@github.com>
2019-05-25 08:36:42 +0200

    Formatting

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
-let deckOfCards = [ "fa-diamond", 
-                    "fa-paper-plane-o", 
-                    "fa-anchor", 
-                    "fa-bolt", 
-                    "fa-cube", 
-                    "fa-leaf", 
-                    "fa-bicycle", 
-                    "fa-bomb", 
-                    "fa-diamond", 
-                    "fa-paper-plane-o", 
-                    "fa-anchor", 
-                    "fa-bolt", 
-                    "fa-cube", 
-                    "fa-leaf", 
-                    "fa-bicycle", 
-                    "fa-bomb"
-                ]
+let deckOfCards = [
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-cube",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bomb",
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-cube",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bomb"
+];
 
-const cardContainer = document.querySelector(".deck")
+const cardContainer = document.querySelector(".deck");
 
 shuffle(deckOfCards);
 
 for (let i = 0; i <= 15; i++) {
-    const cardListItem = document.createElement('li');
-    const card = document.createElement('i');
-    const cardType = deckOfCards[i];
-    // deckOfCards = shuffle(deckOfCards);
-    
-    cardListItem.className = "card";
-    cardListItem.dataset.card = cardType;
-    
-    card.className = "fa " + cardType;
-    cardListItem.appendChild(card);
-    cardContainer.appendChild(cardListItem);
+  const cardListItem = document.createElement("li");
+  const card = document.createElement("i");
+  const cardType = deckOfCards[i];
+  // deckOfCards = shuffle(deckOfCards);
+
+  cardListItem.className = "card";
+  cardListItem.dataset.card = cardType;
+
+  card.className = "fa " + cardType;
+  cardListItem.appendChild(card);
+  cardContainer.appendChild(cardListItem);
 }
 
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -55,14 +55,14 @@ function shuffle(a) {
  */
 
 const allCards = document.querySelectorAll(".card");
-const movesElement = document.querySelector(".moves")
-const restartElement = document.querySelector(".restart")
-const timerElement = document.querySelector(".timer")
-const starsElement = document.querySelector(".stars")
-let openCards = []
-let correctMatches = 0
-let movesCount = 0
-let starsCount = 3
+const movesElement = document.querySelector(".moves");
+const restartElement = document.querySelector(".restart");
+const timerElement = document.querySelector(".timer");
+const starsElement = document.querySelector(".stars");
+let openCards = [];
+let correctMatches = 0;
+let movesCount = 0;
+let starsCount = 3;
 let winningStars = 0;
 let winningTime = 0;
 let winningMoves = 0;
@@ -70,92 +70,116 @@ let minutesLabel = document.getElementById("minutes");
 let secondsLabel = document.getElementById("seconds");
 let totalSeconds = 0;
 
-allCards.forEach( (card) => {
-    card.addEventListener("click", () => {
-        openCards.push(card);
-        if (openCards.length <= 2) {
-            card.classList.add("open", "show"); 
-        }
-        if (openCards.length === 2 && openCards[0].dataset.card != openCards[1].dataset.card) {
-            hideCards()
-            moves()
-            stars()
-        }
-        if (openCards.length === 2 && openCards[0].dataset.card == openCards[1].dataset.card) {
-            openCards.forEach( (card) => {
-                card.classList.add("match")
-            });
-            correctMatches += 1;
-            openCards = [];
-            winningTime = timerElement.textContent;
-            timerElement.textContent = winningTime;
-            stars()
-        }
-        if (correctMatches === 8) {
-            let winningStarsElement = document.querySelector(".winning-stars");
-            let winningTimeElement = document.querySelector(".winning-time");
-            let winningMovesElement = document.querySelector(".winning-moves");
-            winningStarsElement.textContent = starsCount;
-            winningTimeElement.textContent = winningTime;
-            winningMovesElement.textContent = movesCount;
-            startConfetti();
-            MicroModal.show('modal-1')
-        }
-    })
-})
+allCards.forEach(card => {
+  card.addEventListener("click", () => {
+    openCards.push(card);
+    if (openCards.length <= 2) {
+      showCards(card);
+    }
+    if (
+      openCards.length === 2 &&
+      openCards[0].dataset.card != openCards[1].dataset.card
+    ) {
+      hideCards();
+      moves();
+      stars();
+    }
+    if (
+      openCards.length === 2 &&
+      openCards[0].dataset.card == openCards[1].dataset.card
+    ) {
+      matchCards();
+      moves();
+      stars();
+    }
+    if (correctMatches === 8) {
+      gameOver();
+    }
+  });
+});
 
 restartElement.addEventListener("click", () => {
-    newGame()
-})
-
-setInterval(setTime, 1000);
-
-function setTime() {
-  ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-}
-
-function pad(val) {
-  let valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
-  }
-}
-
-hideCards = () => {
-    openCards.forEach( (card) => {
-            card.classList.add("wrong");
-    })
-    setTimeout(() => {
-        openCards.forEach( (card) => {
-            card.classList.remove("open", "show", "wrong");
-        })
-        openCards = [];
-    }, 800);
-}
-
-moves = () => {
-    movesCount += 1;
-    movesElement.textContent = movesCount;
-}
+  newGame();
+});
 
 newGame = () => {
-    movesCount = 0
-    openCards = []
-    movesElement.textContent = movesCount;
-    allCards.forEach( (card) => {
-        card.classList.remove("open", "show", "match", "wrong")
-    })
-    stopConfetti();
-}
+  movesCount = 0;
+  openCards = [];
+  movesElement.textContent = movesCount;
+  allCards.forEach(card => {
+    card.classList.remove("open", "show", "match", "wrong");
+  });
+  stopConfetti();
+  gameTime();
+};
+
+gameTime = () => {
+  setInterval(setTime, 1000);
+
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  }
+
+  function pad(val) {
+    let valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
+};
+
+showCards = card => {
+  card.classList.add("open", "show");
+};
+
+hideCards = () => {
+  openCards.forEach(card => {
+    card.classList.add("wrong");
+  });
+  setTimeout(() => {
+    openCards.forEach(card => {
+      card.classList.remove("open", "show", "wrong");
+    });
+    openCards = [];
+  }, 800);
+};
+
+matchCards = () => {
+  openCards.forEach(card => {
+    card.classList.add("match");
+  });
+  correctMatches += 1;
+  openCards = [];
+  winningTime = timerElement.textContent;
+  timerElement.textContent = winningTime;
+};
+
+gameOver = () => {
+  let winningStarsElement = document.querySelector(".winning-stars");
+  let winningTimeElement = document.querySelector(".winning-time");
+  let winningMovesElement = document.querySelector(".winning-moves");
+  winningStarsElement.textContent = starsCount;
+  winningTimeElement.textContent = winningTime;
+  winningMovesElement.textContent = movesCount;
+  startConfetti();
+  MicroModal.show("modal-1");
+};
+
+moves = () => {
+  movesCount += 1;
+  movesElement.textContent = movesCount;
+};
 
 stars = () => {
-    if (movesCount === 8) {
-        starsElement.children[2].childNodes[0].className += " fa-star-o";
-    } else if (movesCount === 12) {
-        starsElement.children[1].childNodes[0].className += " fa-star-o";
-    }
-}
+  if (movesCount >= 8) {
+    starsElement.children[2].childNodes[0].className += " fa-star-o";
+  } else if (movesCount >= 12) {
+    starsElement.children[1].childNodes[0].className += " fa-star-o";
+  } else if (movesCount >= 15) {
+    starsElement.children[0].childNodes[0].className += " fa-star-o";
+  }
+};
